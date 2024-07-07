e8e20893ee9fc764c2f4a8bbad561007ad8b46a1
Haluk Özdemir <0halukozdemir@gmail.com>
2024-07-07 13:21:24 +0300

    create executor

--- src/executor.c
+++ src/executor.c
@@ -1,46 +1,14 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   executor.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: halozdem <halozdem@student.42istanbul.c    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/07 13:20:14 by halozdem          #+#    #+#             */
+/*   Updated: 2024/07/07 13:20:59 by halozdem         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/minishell.h"
-#include <unistd.h>
 
-void	execute_command(char **args)
-{
-	pid_t pid;
-	int status;
-
-	pid = fork();
-	if (pid == 0)
-	{
-		if (execve(args[0], args, NULL) == -1)
-		{
-			ft_printf("execve hatasiii");
-			exit(EXIT_FAILURE);
-		}
-	}
-	else if(pid < 0)
-	{
-		ft_printf("forkhatasiiiii");
-	}
-	else
-	{
-		do
-		{
-			waitpid(pid, &status, WUNTRACED);
-		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
-
-	}
-}
-
-void	executor(t_minishell *minishell)
-{
-	int i;
-	char **args;
-
-	i = 0;
-	while (minishell->tokens[i])
-	{
-		args = ft_split(minishell->tokens[i], ' ');
-		if (args && args[0])
-			execute_command(args);
-		free(args[i]);
-		i++;
-	}
-}
