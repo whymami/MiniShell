eef7a686da8913bdcd321f5ba843c9a9a9fefb66
Batuhan TANIR <batuhan.tanir00@gmail.com>
2024-07-11 15:37:09 +0300

    Refactor export function to accept env_data instead of separate key and value parameters

--- src/builtin/export.c
+++ src/builtin/export.c
@@ -6,20 +6,16 @@
 /*   By: btanir <btanir@student.42istanbul.com.tr>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/07 18:55:17 by halozdem          #+#    #+#             */
-/*   Updated: 2024/07/08 16:14:36 by btanir           ###   ########.fr       */
+/*   Updated: 2024/07/11 15:35:58 by btanir           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	export(t_minishell *minishell, char *key, char *value)
+void	export(t_minishell *minishell, char *env_data)
 {
-	t_env	*env_data;
 	t_dlist	*new;
 
-	env_data = malloc(sizeof(env_data));
-	env_data->key = key;
-	env_data->value = value;
 	new = dlist_new(env_data);
 	dlist_add_back(&minishell->env, new);
 }
